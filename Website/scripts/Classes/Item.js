7a71d44c1ee8a9ad3e9ffc5cea8170f518a2569c
it170135 <quirin.ecker@bajupa.com>
2019-05-29 12:50:22 +0200

    added item counter and item ID

--- Website/scripts/Classes/Item.js
+++ Website/scripts/Classes/Item.js
@@ -1,8 +1,12 @@
+let numberOfItems = 0;
+
 class Item {
     constructor(name, catigory, icon) {
+        this.id = numberOfItems;
         this.name = name;
         this.catigory = catigory;
         this.icon = icon;
+        numberOfItems++;
     }
 }
 
