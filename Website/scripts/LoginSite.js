d1dabe2fc122fff8261eb117e6baed830e922e13
it170135 <quirin.ecker@bajupa.com>
2019-06-04 10:24:29 +0200

    style for list-edit and list-delete button implemented

--- Website/scripts/LoginSite.js
+++ Website/scripts/LoginSite.js
@@ -30,9 +30,9 @@ window.addEventListener('load', () => {
                         loginWave.classList.add('enter-animation');
 
                         setTimeout(() => {
-                            localStorage.setItem("username", item.username)
-                            localStorage.setItem("password", item.password)
-                            localStorage.setItem("email", item.email)
+                            localStorage.setItem("username", item.username);
+                            localStorage.setItem("password", item.password);
+                            localStorage.setItem("email", item.email);
                             localStorage.setItem("enums", item.enums);
                             window.location.href = "/index.html";
                         }, 2000)
